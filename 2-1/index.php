c0fe2ad862c4fc7196efc6000a47efebfc54e19f
YumiMatsuda <yumimin1xox1@gmail.com>
2020-07-08 21:26:16 +0900

    【課題提出】第二章Hello World
    index.phpの提出

---
+++ 2-1/index.php
@@ -0,0 +1,29 @@
+<?php
+?>
+<?pphp
+/*
+PHPの
+研修
+カリキュラム
+2-1
+*/
+?>
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+
+<?php
+echo "1. 開発環境・検証環境・本番環境"."<br>";
+echo "・開発環境：機器やソフトウェア、システムを開発するために必要な機材やソフトウェアなどの組み合わせ。"."<br>";
+echo "また、それらを組み合わせて構成された、開発者の作業環境。"."<br>";
+echo "・検証環境：検証環境は、開発環境で作られたシステムをテストするための工程のこと。"."<br>";
+echo "・本番環境：システム開発に関する用語で、システムが製品として実際に稼動している環境のこと。"."<br>";
+echo "2. 基本設計書・詳細設計書"."<br>";
+echo "・基本設計書：顧客の要件を実現するためのシステム構成や機能を具体化する工程を記載した資料のこと"."<br>";
+echo "・詳細設計書：基本設計で決められた動きを「どうやって実現するか？」を説明した資料"."<br>";
+echo "3. Ajax"."<br>";
+echo "・Ajax:あるWebページを表示した状態のまま、別のページや再読込などをせずにWebサーバ側と通信を行い、"."<br>";
+echo "表示内容を変更する手法。ページ上でプログラムを実行できるJavaScriptの拡張機能を使っている。"."<br>";
+?><img class="alignnone size-full wp-image-371" 
+src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" 
+alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
