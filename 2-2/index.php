b57973bd62978d54d9daa0a07b0869151bc08648
YumiMatsuda <yumimin1xox1@gmail.com>
2020-07-11 11:00:14 +0900

    【課題提出】第二章変数とデータ型
    index.phpの提出

---
+++ 2-2/index.php
@@ -0,0 +1,8 @@
+<?php
+echo "1. API:Application Programming Interfaceの略。<br>";
+echo "ソフトウェアの機能を共有できる仕組み。<br>";
+echo "ソフトウェアからOSの機能を利用するための仕様またはインターフェースの総称で、<br>";
+echo "アプリケーションの開発を容易にするためのソフトウェア資源のこと。<br>";
+echo "2. SVN（Subversion）:正式名称は「Apache Subversion」。<br>";
+echo "オープンソースの集中型バージョン管理システムのこと。<br>";
+?>
