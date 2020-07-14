fe1b6e134c4a15286339bbba07d7df21099ebd96
YumiMatsuda <yumimin1xox1@gmail.com>
2020-07-15 00:27:09 +0900

    【課題提出】第二章配列、連想配列
    index.php

--- 2-7/index.php
+++ 2-7/index.php
@@ -1,4 +1,18 @@
 <?php
+$colors = ["red" => "赤", "blue" => "青", "green" => "緑"];
+
+var_dump($colors);
+
+echo "<br>";
+
+$colors = ["red" => "赤", "blue" => "青", "green" => "緑", "yellow" => "黄色"];
+
+var_dump($colors);
+
+echo "<br>";
+
+// IT用語 SESに向けて準備しよう
+echo "IT用語 SESに向けて準備しよう<br>";
 echo "1. プルリクエスト（マージリクエスト）：コードの変更をレビュワーに通知し、マージを依頼する機能。<br>";
 echo "2. Git Flow：Gitの機能であるブランチを活用したGitの開発手法。<br>";
 echo "それぞれ役割が振られているmaster, release, develop, feature, hot-fixの5つのブランチを使い分けて、開発を進める。<br>";
