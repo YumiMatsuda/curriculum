dd544c327678e918cfc4c058746a81e3bf5178af
YumiMatsuda <yumimin1xox1@gmail.com>
2020-07-24 18:45:50 +0900

    【課題提出】第二章EXTRA問題
    index.php
    index2.php
    の作成

--- 2-17/index2.php
+++ 2-17/index2.php
@@ -3,17 +3,22 @@
 date_default_timezone_set('Asia/Tokyo');
 
 //現在時刻を取得する
-$time = date("H");
-echo "今".$time."時台です。<br>";
+$time = date ("H");
+
+//型変換する。文字型を整数型に変換する
+$intTime = intval ($time);
+echo "今".$intTime."時台です。<br>";
 
 //取得した現在時刻によって表示させる挨拶を変更する
 //もし、現在時刻が4時〜11時の場合は「おはようございます！」と表示
-if ($time >= 4 && $time <= 11) {
-    echo "おはようございます！";
+if ($intTime >= 4 && $intTime <= 11) {
+    echo "おはようございます！<br>";
 //現在時刻が12時〜15時の場合は「こんにちは！」と表示
-} elseif ($time >=12 && $time <= 15) {
-    echo "こんにちは！";
+} elseif ($intTime >=12 && $intTime <= 15) {
+    echo "こんにちは！<br>";
 //上記以外の時間の場合は「こんばんは！」と表示
 } else {
-    echo "こんばんは！";
+    echo "こんばんは！<br>";
 }
+
+var_dump($intTime, $time);
