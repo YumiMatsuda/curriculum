7fa777ef83979ad4fc77fe1efe70b274d9b6860d
YumiMatsuda <yumimin1xox1@gmail.com>
2020-10-19 17:21:14 +0900

    【課題提出】4章ブログを作ってみよう
    signUp.php
    login.php
    main.php
    create_post.php
    edit_post.php
    edit_done_post.php
    delete_post.php
    detail_post.php
    create_comment.php
    logout.php
    db_connect.php
    の提出。

---
+++ 4-1/signUp.php
@@ -0,0 +1,57 @@
+<?php
+// db_connect.phpの読み込み
+include_once("db_connect.php");
+
+// エラーメッセージ、登録完了メッセージの初期化
+$errorMessage = "";
+$signUpMessage = "";
+
+// ログインボタンが押された場合
+if (isset($_POST["signUp"])) {
+    if (!empty($_POST["name"]) && !empty($_POST["password"])) {
+        // 入力したユーザIDとパスワードを格納
+        $name = $_POST["name"];
+        $password = $_POST["password"];
+
+        // 2. ユーザIDとパスワードが入力されていたら認証する
+        // %s：文字列、sprintf：変数から文字列を作り出す。
+        $dsn = sprintf('mysql: host=%s; dbname=%s; charset=utf8', PDO_DSN, DB_DATABASE);
+
+        $pdo = db_connect();
+
+        // 3. エラー処理
+        try {
+            $stmt = $pdo->prepare("INSERT INTO users(name, password) VALUES (?, ?)");
+
+            $stmt->execute(array($name, password_hash($password, PASSWORD_DEFAULT)));  // パスワードのハッシュ化を行う（今回は文字列のみなのでbindValue(変数の内容が変わらない)を使用せず、直接excuteに渡しても問題ない）
+            $userid = $pdo->lastinsertid();  // 登録した(DB側でauto_incrementした)IDを$useridに入れる
+
+            $signUpMessage = '登録が完了しました。';  // ログイン時に使用するIDとパスワード
+            } catch (PDOException $e) {
+            $errorMessage = 'データベースエラー';
+            // $e->getMessage() でエラー内容を参照可能（デバック時のみ表示）
+            echo $e->getMessage();
+            }
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>新規登録画面</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+<body>
+    <h1>新規登録</h1>
+    <form method="POST" action="">
+    <div><?php echo htmlspecialchars($errorMessage, ENT_QUOTES); ?></div>
+    <div><?php echo htmlspecialchars($signUpMessage, ENT_QUOTES); ?></div>
+        user:<br>
+        <input type="text" name="name" id="name">
+        <br>
+        password:<br>
+        <input type="password" name="password" id="password"><br>
+        <input type="submit" value="submit" id="signUp" name="signUp">
+    </form>
+</body>
+</html>
